6cc15d9f592c4912f22b3fbf333a6b0b5ee82da2
Garv nanwani <garvnanwani16@gmail.com>
2020-10-16 14:08:21 +0530

    added tailwind and postcss to react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './assests/css/style.css';
 
 function App() {
     return (
-        <div>Hello World</div>
+        <div className="bg-gray-600 text-white padding-5" >Hello World</div>
     )
 }
 
